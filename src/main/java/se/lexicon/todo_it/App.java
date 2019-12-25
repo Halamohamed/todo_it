1865e8df3aafed66d0f0f3613723fff64ca172a2
Hala Mohammed <halaalimohammed@gmail.com>
2019-12-25 12:51:34 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/todo_it/App.java
@@ -0,0 +1,13 @@
+package se.lexicon.todo_it;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
